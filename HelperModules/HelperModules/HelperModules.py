72bd2c127e8800bda41813dffe8bdac41218a325
martinarnoldi <martin_arnoldi@hotmail.com>
2017-09-05 10:07:20 +0200

    MNA

---
+++ HelperModules/HelperModules/HelperModules.py
@@ -0,0 +1,17 @@
+
+import os
+import datetime
+import time
+
+def Log(logEntry,logfilePath,maxLogSizeInBytes):
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    #logfilePath = "C:\\temp\\debug.log"
+    #logEntry = "TEST"
+
+    if os.path.getsize(logfilePath) > maxLogSizeInBytes * 1024:
+      os.remove(logfilePath)
+
+    file = open(logfilePath,'a')
+    file.write(timestamp + ' - ' + logEntry)
+    file.close()
