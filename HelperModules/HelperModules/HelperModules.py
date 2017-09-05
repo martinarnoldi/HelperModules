ac6ed366540760dd500d42e22fa3437bc1a5d21c
martinarnoldi <martin_arnoldi@hotmail.com>
2017-09-05 10:21:42 +0200

    MNA

--- HelperModules/HelperModules/HelperModules.py
+++ HelperModules/HelperModules/HelperModules.py
@@ -8,10 +8,10 @@ def Log(logEntry,logfilePath,maxLogSizeInBytes):
     timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     #logfilePath = "C:\\temp\\debug.log"
     #logEntry = "TEST"
-
-    if os.path.getsize(logfilePath) > maxLogSizeInBytes * 1024:
-      os.remove(logfilePath)
+    if os.path.isfile(logfilePath): 
+       if os.path.getsize(logfilePath) > maxLogSizeInBytes * 1024:
+          os.remove(logfilePath)
 
     file = open(logfilePath,'a')
-    file.write(timestamp + ' - ' + logEntry)
+    file.write(timestamp + ' - ' + logEntry + "\n")    
     file.close()
---
+++ HelperModules/HelperModules/MainTest.py
@@ -0,0 +1,8 @@
+import HelperModules as h
+logfilePath = "c:\\temp\\python.log"
+maxLogSizeInBytes = 1024
+
+while True:
+    h.Log("MNA TEST",logfilePath,maxLogSizeInBytes)
+
+
